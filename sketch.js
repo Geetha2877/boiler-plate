3f9084a8bac8a1efcfa95a79112291bf2d894b1f
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-04-20 18:51:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,20 +2,20 @@
  const World = Matter.World;
  const Bodies=Matter.Bodies;
  var engine,world;
- var object; 
+ var ground; 
 
  function setup() {
-  var canvas =createCanvas(800,400);
+  var canvas =createCanvas(400,400);
   engine=Engine.create();
   world=enigne.world;
-  object=Bodies.rectangle(200,100,50,50);
-  World.add(world,object);
-  console.log(object);
+  ground=Bodies.rectangle(200,390,200,20);
+  World.add(world,ground);
+  console.log(ground);
 }
 
 function draw() {
   background(0);  
   Engine.update(engine);
   rectMode(CENTER);
-  rect(object.position.x,object.position.y,50,50);
+  rect(ground.position.x,ground.position.y,400,20);
 } 
